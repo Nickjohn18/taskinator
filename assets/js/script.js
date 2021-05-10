15d8f8ad4c2501daf7cb26937948b1c1b1381e58
Nickjohn18 <81334326+Nickjohn18@users.noreply.github.com>
2021-05-10 12:20:46 -0400

    innerHTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,21 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
     event.preventDefault();
-    
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // create new list items
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    // create div to hold task infor and list item
+    var taskInfoEl = document.createElement("div");
+    //give name to div class
+    taskInfoEl.classname = "task-info"
+    // add content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class= 'task=type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
     tasksToDoEl.appendChild(listItemEl);
 };
 
